e547d95752d2a896f277bf59dc5ba0c92df2e942
pgrwe <aldreichampadu@outlook.com>
2023-05-27 19:29:41 -0400

    fixed typo p4

--- vocab-vault/src/Navbar.jsx
+++ vocab-vault/src/Navbar.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import React, { useState } from 'react';
-import logo from '../vocab-vault-logo.jpeg';
+import logo from './images/vocab-vault-logo.jpeg';
 
 
 export default function Navbar() {
