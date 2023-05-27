7894db66c3e130ec7da0b6c1a5c4ff64f122aae6
supersillyswiper <dramirez2@binghamton.edu>
2023-05-26 20:25:41 -0400

    added header to website

---
+++ vocab-vault/src/header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <header className="App-header">
+      <div class="search-container">
+    <form>
+      <input type="text" placeholder="Search..." name="search" />
+      <button type="submit">Search</button>
+    </form>
+          </div>
+        
+
+        
+          
+         
+        
+      </header>
+      </div>
+  );
+}
+
+export default Header;
